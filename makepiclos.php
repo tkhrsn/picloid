4c13719940c7036ef6bfe7ac4feca5538464bf87
tkhrsn <tkhrsn@yahoo.co.jp>
2017-10-03 22:40:16 +0900

    get line number.

--- makepiclos.php
+++ makepiclos.php
@@ -2,6 +2,7 @@
 $sides = 25;
 $fields = array();
 
+// フィールド作成
 for ($i = 0; $i < $sides; $i++) {
     for ($j = 0; $j < $sides; $j++) {
         $fields[$i][$j] = empty(rand(0, 1));
@@ -13,5 +14,22 @@ foreach ($fields as $line) {
     foreach ($line as $paint) {
         echo $paint ? '■' : '□';
     }
+    echo "\n";
+}
+
+// 横の数字を取得
+foreach ($fields as $line) {
+    $line_cnts = array();
+    $line_cnts[0] = 0;
+    foreach ($line as $i => $paint) {
+        if ($paint) {
+            $line_cnts[count($line_cnts)-1]++;
+        } else {
+            if (!empty(end($line_cnts))) {
+                $line_cnts[count($line_cnts)] = 0;
+            }
+        }
+    }
+    echo implode(' ', array_filter($line_cnts));
     echo "\n";
 }
