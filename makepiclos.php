4f97222cafbdbbf9587cf3dbf009285dd859f3e5
tkhrsn <tkhrsn@yahoo.co.jp>
2017-10-03 22:43:48 +0900

    add column number.

--- makepiclos.php
+++ makepiclos.php
@@ -1,11 +1,14 @@
 <?php
 $sides = 25;
 $fields = array();
+$fields_turned = array();
 
 // フィールド作成
 for ($i = 0; $i < $sides; $i++) {
     for ($j = 0; $j < $sides; $j++) {
-        $fields[$i][$j] = empty(rand(0, 1));
+        $paint = empty(rand(0, 1));
+        $fields[$i][$j] = $paint;
+        $fields_turned[$j][$i] = $paint;
     }
 }
 
@@ -19,6 +22,25 @@ foreach ($fields as $line) {
 
 // 横の数字を取得
 foreach ($fields as $line) {
+    $line_cnts = array();
+    $line_cnts[0] = 0;
+    foreach ($line as $i => $paint) {
+        if ($paint) {
+            $line_cnts[count($line_cnts)-1]++;
+        } else {
+            if (!empty(end($line_cnts))) {
+                $line_cnts[count($line_cnts)] = 0;
+            }
+        }
+    }
+    echo implode(' ', array_filter($line_cnts));
+    echo "\n";
+}
+
+echo "\n";
+
+// 縦の数字を取得
+foreach ($fields_turned as $line) {
     $line_cnts = array();
     $line_cnts[0] = 0;
     foreach ($line as $i => $paint) {
