18062813167f617cca982349303ad585c75faa5f
tkhrsn <tkhrsn@yahoo.co.jp>
2017-10-04 23:18:19 +0900

    make csv doing.

--- makepiclos.php
+++ makepiclos.php
@@ -5,19 +5,17 @@ const SIDES_DEFAULT = 5;
 $sides = get_sides($argv);
 list($field, $field_turned) = make_field($sides);
 
-// フィールドを出力
-foreach ($field as $line) {
-    foreach ($line as $paint) {
-        echo $paint ? '■' : '□';
-    }
-    echo "\n";
-}
-
-// 横の数字を取得
+// 横の数列を取得
 $line_cnts = get_line_cnts($field);
 
-// 縦の数字を取得
+// 縦の数列を取得
 $col_cnts = get_line_cnts($field_turned);
+
+// 問題CSVを出力
+put_question_csv($line_cnts, $col_cnts);
+
+// 答えCSVを出力
+put_answer_csv($field);
 
 
 // methods
@@ -43,7 +41,7 @@ function get_sides($argv) {
 }
 
 /**
- * フィールド作成
+ * フィールド要素作成
  * @param type $sides
  * @return type
  */
@@ -62,6 +60,26 @@ function make_field($sides) {
 }
 
 /**
+ * 答えフィールドを作成
+ * @param type $field
+ * @return type
+ */
+function put_answer_csv($field) {
+    $txt = '';
+    foreach ($field as $line) {
+        $answer_line = array();
+        foreach ($line as $paint) {
+            $answer_line[] = $paint ? '■' : '□';
+        }
+        $txt .= implode(',', $answer_line)."\n";
+    }
+
+    // TODO answer.csvに出力する
+    echo $txt;
+}
+
+
+/**
  * 縦横の数字カウントを取得
  * @param type $field
  * @return type
@@ -71,7 +89,7 @@ function get_line_cnts($field) {
     foreach ($field as $line) {
         $line_cnts = array();
         $line_cnts[0] = 0;
-        foreach ($line as $i => $paint) {
+        foreach ($line as $paint) {
             if ($paint) {
                 $line_cnts[count($line_cnts)-1]++;
             } else {
@@ -86,4 +104,54 @@ function get_line_cnts($field) {
     return $lines;
 }
 
-// 横の数字を取得
+/**
+ * 問題CSV出力
+ * @param type $line_cnts
+ * @param type $col_cnts
+ */
+function put_question_csv($line_cnts, $col_cnts) {
+    // 縦横数列の最大値をそれぞれ取得し、最大値まで配列の空白を埋める
+    $line_max = get_max_array_num($line_cnts);
+    $col_max  = get_max_array_num($col_cnts);
+
+    $line_padded = pad($line_cnts, $line_max);
+    $col_padded  = pad($col_cnts,  $col_max);
+    var_dump($line_padded);
+    var_dump($col_padded);die;
+
+    // TODO 縦数列を縦表示にするため反転させる
+
+    // TODO CSV用配列作成
+
+    // TODO 出力
+}
+
+/**
+ * 二次元配列の最大要素数を取得
+ * @param type $cnts
+ * @return type
+ */
+function get_max_array_num($cnts) {
+    $max = 0;
+    foreach ($cnts as $cnt) {
+        $max = max($max, count($cnt));
+    }
+
+    return $max;
+}
+
+/**
+ * 二次元配列の全ての行に空白文字列を埋めて同じ要素数にする
+ * @param type $cnts
+ * @param type $max
+ * @return type
+ */
+function pad($cnts, $max) {
+    $pads = array();
+    foreach ($cnts as $cnt) {
+        $pad_num = $max - count($cnt);
+        $pads[]  = array_merge(array_fill(0, $pad_num, ''), $cnt);
+    }
+
+    return $pads;
+}
